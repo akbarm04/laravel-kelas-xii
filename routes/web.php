ad63800db81141c0b35ef89b709769ae667d1347
Nik Net <dyandra.wahyu@gmail.com>
2024-11-10 17:35:50 +0700

    feat(index and create cast) : Display the cast list and add a cast

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
     FilmController,
+    CastController,
 };
 
 Route::get('/', [FilmController::class, 'movieHome'])->name('home');
 Route::get('/movies', [FilmController::class, 'movies'])->name('movies');
 Route::get('/movies/{film}', [FilmController::class, 'show'])->name('movies.show');
 Route::get('/movies/genre/{genre}', [FilmController::class, 'moviesByGenre'])->name('genre');
+
+Route::resource('cast', CastController::class);
+Route::put('/cast/{id}', [CastController::class, 'update'])->name('cast.update');
+Route::post('/cast/store', [CastController::class, 'store'])->name('cast.store');
