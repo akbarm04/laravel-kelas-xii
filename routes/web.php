6a805d4891c1ec4b4c19565688630c4c1bd3914d
akbarm04 <am8343969@gmail.com>
2024-11-10 19:15:22 +0700

    add routes cast

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@ Route::get('/movies/{film}', [FilmController::class, 'show'])->name('movies.show
 Route::get('/movies/genre/{genre}', [FilmController::class, 'moviesByGenre'])->name('genre');
 
 Route::resource('cast', CastController::class);
-Route::put('/cast/{id}', [CastController::class, 'update'])->name('cast.update');
 Route::post('/cast/store', [CastController::class, 'store'])->name('cast.store');
+Route::get('/cast/{id}', [CastController::class, 'show'])->name('cast.show');    
+Route::get('/cast/{id}/edit', [CastController::class, 'edit'])->name('cast.edit');
+Route::put('/cast/{id}', [CastController::class, 'update'])->name('cast.update');
